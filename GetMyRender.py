5fe2575047c20b7e888c2aa1f70fe2dec4e67871
Yeswanthgadde9 <yeswanthgyc@gmail.com>
2023-01-28 13:47:48 +0530

    Initial commit

---
+++ GetMyRender.py
@@ -0,0 +1,93 @@
+import sys
+import json
+from PySide2.QtWidgets import *
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from GetMyRenderUI import Ui_GetMyRender
+
+
+class Render(QWidget, Ui_GetMyRender):
+    def __init__(self):
+        super(Render, self).__init__()
+
+        self.setupUi(self)
+        self.add_shot()
+        self.shot_data = file_name ='{}\\config\\shot_data.json'.format(os.path.dirname(__file__))
+        self.Render_Data.itemClicked.connect(self.display_shot_name)
+        self.Import_Render.clicked.connect(self.get_render)
+        self.ImportScript.clicked.connect(self.get_script)
+        self.Render_Data.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.Render_Data.customContextMenuRequested.connect(self.custom_context_menu)
+
+    def add_shot(self):
+        with open(self.shot_data, "r") as file:
+            self.shot_data = json.load(file)
+            for shot in self.shot_data:
+                self.Render_Data.addItem(shot)
+
+    def display_shot_name(self, shot):
+        self.shot_name = shot.text()
+        self.Shotinfo.setText(self.shot_name)
+        self.frame_range = "{}-{}".format(self.shot_data[self.shot_name]['start_frame'],
+                                          self.shot_data[self.shot_name]['end_frame'])
+        self.Framerangeinfo.setText(self.frame_range)
+        if self.shot_data[self.shot_name]['chanels'] == 'all':
+            self.Channelinfo.setText('rgba')
+        else:
+            channels = self.shot_data[self.shot_name]['chanels']
+            self.Channelinfo.setText(channels)
+        thumbnail_path = self.shot_data[self.shot_name]['thumbnail_path']
+        self.Thumbnail_label.setPixmap(thumbnail_path)
+        self.render_path = ("{} {}".format(self.shot_data[self.shot_name]['path'], self.frame_range))
+        self.script_path = self.shot_data[self.shot_name]['script']
+        format = self.shot_data[self.shot_name]['resolution']
+        self.Resolutioninfo.setText(format)
+        date = self.shot_data[self.shot_name]['date']
+        date = date.split('-')[0]
+        self.Dateinfo.setText(date)
+        extension = self.shot_data[self.shot_name]['path']
+        extension = extension.split('.')[-1]
+        self.Extensioninfo.setText(extension)
+        time = self.shot_data[self.shot_name]['date']
+        time = time.split('-')[1]
+        self.Timeinfo.setText(time)
+
+    def get_render(self):
+        nukescripts.clear_selection_recursive()
+        read = nuke.createNode("Read")
+        read.knob("file").fromUserText(self.render_path)
+
+    def custom_context_menu(self, pos):
+        self.menu = QMenu()
+        open = self.menu.addAction('Open')
+        delete = self.menu.addAction('Delete')
+        action = self.menu.exec_(self.Render_Data.mapToGlobal(pos))
+        item_name = self.Render_Data.currentItem().text()
+        print(item_name)
+        if action == open:
+            paths = self.shot_data[item_name]['script']
+            paths = paths.split('/')
+            latest_path = paths.pop()
+            f_path = '/'.join(paths)
+            QDesktopServices.openUrl(QtCore.QUrl.fromLocalFile(f_path))
+        elif action == delete:
+            print("deleted")
+            del self.shot_data[item_name]
+            print(self.shot_data)
+            self.write_json_data(self.shot_data)
+
+    def write_json_data(self, data):
+        with open(self.shot_data, "w") as file:
+            json.dump(self.shot_data, file, indent=4)
+
+    def get_script(self):
+        nukescripts.clear_selection_recursive()
+        nuke.nodePaste(self.script_path)
+
+
+def main():
+    main.widgets = Render()
+    main.widgets.show()
+
+
+main()
---
+++ Get_My_Render_Export_Data.py
@@ -0,0 +1,97 @@
+import json
+import os
+from datetime import datetime
+
+
+class GetMyData:
+    def __init__(self):
+        data = self.get_details()
+        self.make_thumbnail()
+        self.write_json(data)
+
+    def get_details(self):
+        details = {}
+        self.node = nuke.thisNode()
+        self.path = self.node['file'].value()
+        script = nuke.root()
+        format = self.node.format().name()
+        script = script['name'].value()
+        self.c_time = datetime.now()
+        self.channels = self.node['channels'].value()
+        self.start_frame = self.node.firstFrame()
+        if self.start_frame <= 9:
+            self.start_frame = '000'"{}".format(self.start_frame)
+        elif 9 < self.start_frame <= 99:
+            self.start_frame = '00'"{}".format(self.start_frame)
+        elif 99 < self.start_frame <= 999:
+            self.start_frame = '0'"{}".format(self.start_frame)
+        else:
+            self.start_frame = self.start_frame
+        self.end_frame = self.node.lastFrame()
+        if self.end_frame <= 9:
+            self.end_frame = '000'"{}".format(self.end_frame)
+        elif 9 < self.end_frame <= 99:
+            self.end_frame = '00'"{}".format(self.end_frame)
+        elif 99 < self.end_frame <= 999:
+            self.end_frame = '0'"{}".format(self.end_frame)
+        else:
+            self.end_frame = self.end_frame
+        self.shot_name = os.path.basename(self.path).title()
+        self.shot_name = self.shot_name.split('.')[0]
+        self.thumbnail_path = '{}\\thumbnails\\{}_{}.png'.format(os.path.dirname(__file__), self.shot_name, str(self.start_frame))
+        details[self.shot_name] = {"path": self.path, "script": script, "start_frame": self.start_frame,
+                                   "end_frame": self.end_frame, "thumbnail_path": self.thumbnail_path,
+                                   "chanels": self.channels, "date": datetime.now().strftime('%d/%m/%Y-%H:%M:%S'),
+                                   "resolution": format}
+        return details
+
+    def make_thumbnail(self):
+        del_nodes = []
+        read_path = self.path.replace('%04d', str(self.start_frame))
+        read = nuke.createNode('Read')
+        read['file'].setValue(read_path)
+        del_nodes.append(read)
+        reformat = nuke.nodes.Reformat(format='square_512', pbb=True, resize='fill', black_outside=True)
+        reformat.setInput(0, read)
+        del_nodes.append(reformat)
+        write = nuke.nodes.Write(file=self.thumbnail_path)
+        write['file_type'].setValue('png')
+        write.setInput(0, reformat)
+        del_nodes.append(write)
+        try:
+            nuke.execute(write, 1, 1)
+            for nodes in del_nodes:
+                nuke.delete(nodes)
+        except:
+            for nodes in del_nodes:
+                nuke.delete(nodes)
+
+    def write_json(self, new_data):
+        file_name ='{}\\config\\shot_data.json'.format(os.path.dirname(__file__))
+        file_data = {}
+        try:
+            with open(filename, 'r') as data_file:
+                file_data = json.load(data_file)
+        except:
+            pass
+        shot_count = file_data.keys()
+        if len(shot_count) > 10:
+            time_val = dict()
+            for key, val in file_data.items():
+                time = val['Date']
+                time_val[key] = time
+                l_time = min(time_val.values())
+                l_val = time_val.keys()[time_val.values().index(l_time)]
+                l_thumb = file_data[l_val]['thumbnail_path']
+            del file_data[l_val]
+            os.remove(l_thumb)
+            file_data.update(new_data)
+            with open(filename, 'w') as file_data:
+                json.dump(file_data, file_data, indent=4)
+        else:
+            file_data.update(new_data)
+            with open(filename, 'w') as write_file:
+                json.dump(file_data, write_file, indent=4)
+
+
+GetMyData()
